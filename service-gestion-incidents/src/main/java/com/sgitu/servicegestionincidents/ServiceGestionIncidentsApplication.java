9866dabfa5f5fe1a4bc559871f38994f3ea32600
ami-ne001 <ami-ne001@users.noreply.github.com>
2026-05-01 23:58:52 +0100

    chore: setup service-gestion-incidents project and dependencies, add root gitignore

---
+++ service-gestion-incidents/src/main/java/com/sgitu/servicegestionincidents/ServiceGestionIncidentsApplication.java
@@ -0,0 +1,13 @@
+package com.sgitu.servicegestionincidents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGestionIncidentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGestionIncidentsApplication.class, args);
+    }
+
+}
